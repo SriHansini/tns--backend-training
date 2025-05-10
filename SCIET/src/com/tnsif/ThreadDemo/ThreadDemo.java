4448abd4b6813150aa4ae5f560f7fb3703cd0e06
SriHansini <hansini0812@gmail.com>
2025-05-10 14:36:55 +0530

    Thread Demo

---
+++ SCIET/src/com/tnsif/ThreadDemo/ThreadDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.ThreadDemo;
+public class ThreadDemo extends Thread{
+	
+	public void run() {
+		for(int i=0;i<4;i++) {
+			System.out.println(Thread.currentThread());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+
---
+++ SCIET/src/com/tnsif/ThreadDemo/ThreadDemoMain.java
@@ -0,0 +1,12 @@
+package com.tnsif.ThreadDemo;
+public class ThreadDemoMain {
+	public static void main(String[] args) {
+      ThreadDemo t1=new ThreadDemo();
+     
+     // ThreadDemo t2=new ThreadDemo();
+      t1.start();
+		//t2.start();	
+	}
+}
+
+
